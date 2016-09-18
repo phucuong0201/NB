493168d3ee68fb8a258ac4b536c3310718d92e01
phucuong0201 <phucuong0201@gmail.com>
2016-09-18 12:50:38 +0700

    Tinh toan xong xac suat tuong ung voi tung truong ben trong file log

--- nb.py
+++ nb.py
@@ -74,11 +74,11 @@ for i in count_time_attack:
     ptime_attack.append(float(i)/float(attack_line))
 for i in count_time_attack_items:         # fix 14.9
     time_label_attack.append(i[0])
-list_ptime = dict(zip(time_list_attack,ptime_attack)) # fix 14.9
+list_ptime_attack = dict(zip(time_list_attack,ptime_attack)) # fix 14.9
 for i in time_list_attack:
-    for j in range(len(list_ptime.keys())):
-        if i == list_ptime.keys()[j]:
-            time_compare_attack.append(list_ptime.values()[j])
+    for j in range(len(list_ptime_attack.keys())):
+        if i == list_ptime_attack.keys()[j]:
+            time_compare_attack.append(list_ptime_attack.values()[j])
 # pprint (time_compare_attack)
 # pprint(time_compare_attack)
 #xac suat cua truong method cho attack log va so sanh gia tri xac suat voi thoi gian tuong ug
@@ -86,54 +86,92 @@ for i in count_method_attack:
     pmethod_attack.append(float(i)/float(attack_line))
 for i in count_method_attack_items:         # fix 14.9
     method_label_attack.append(i[0])
-list_pmethod = dict(zip(method_list_attack,pmethod_attack)) # fix 14.9
+list_pmethod_attack = dict(zip(method_list_attack,pmethod_attack)) # fix 14.9
 for i in method_list_attack:
-    for j in range(len(list_pmethod.keys())):
-        if i == list_pmethod.keys()[j]:
-            method_compare_attack.append(list_pmethod.values()[j])
+    for j in range(len(list_pmethod_attack.keys())):
+        if i == list_pmethod_attack.keys()[j]:
+            method_compare_attack.append(list_pmethod_attack.values()[j])
 # pprint (method_compare_attack)
-
 for i in count_query_attack:
     pquery_attack.append(float(i)/float(attack_line))
 for i in count_query_attack_items:         # fix 14.9
     query_label_attack.append(i[0])
-list_pquery = dict(zip(query_label_attack,pquery_attack)) # fix 14.9
+list_pquery_attack = dict(zip(query_list_attack,pquery_attack))
 for i in query_list_attack:
-    for j in range(len(list_pquery.keys())):
-        if i == list_pquery.keys()[j]:
-            query_compare_attack.append(list_pquery.values()[j])
-# pprint(query_compare_attack)
+    for j in range(len(list_pquery_attack.keys())):
+        if i == list_pquery_attack.keys()[j]:
+            query_compare_attack.append(list_pquery_attack.values()[j])
+pprint(query_compare_attack)
 
 for i in count_status_attack:
     pstatus_attack.append(float(i)/float(attack_line))
 for i in count_status_attack_items:         # fix 14.9
     status_label_attack.append(i[0])
-list_pstatus_code = dict(zip(status_label_attack,pstatus_attack)) # fix 14.9
+list_pstatus_code_attack = dict(zip(status_label_attack,pstatus_attack)) # fix 14.9
 for i in status_list_attack:
-    for j in range(len(list_pstatus_code.keys())):
-        if i == list_pstatus_code.keys()[j]:
-            status_compare_attack.append(list_pstatus_code.values()[j])
+    for j in range(len(list_pstatus_code_attack.keys())):
+        if i == list_pstatus_code_attack.keys()[j]:
+            status_compare_attack.append(list_pstatus_code_attack.values()[j])
 csv_attackfile.close()
 
 with open('normal_log.csv' , "r+") as csv_normalfile:
     reader_normallog = csv.reader(csv_normalfile)
-    for line1 in reader_normallog:
-        time_list_normal.append(str(line1[0]))
-        method_list_normal.append(str(line1[1]))
+    for line in reader_normallog:
+        time_list_normal.append(str(line[0]))
+        method_list_normal.append(str(line[1]))
         query_list_normal.append(str(line[2]))
         status_list_normal.append(str(line[3]))
-count_time_normal = Counter(time_list_normal).values()
-count_method_normal = Counter(method_list_normal).values()
-count_query_normal = Counter(query_list_normal).values()
-count_status_normal = Counter(status_list_normal).values()
+count_time_normal = Counter(time_list_normal).values()      #time
+count_time_normal_items = Counter(time_list_normal).items()
+count_method_normal = Counter(method_list_normal).values()  #method
+count_method_normal_items = Counter(method_list_normal).items()
+count_query_normal = Counter(query_list_normal).values()    #query
+count_query_normal_items = Counter(query_list_normal).items()
+count_status_normal = Counter(status_list_normal).values()  #status
+count_status_normal_items = Counter(status_list_normal).items()
+
+#xac suat cua truong thoi gian cho normal log va so sanh gia tri xac suat voi thoi gian tuong ung
 for i in count_time_normal:
-    #xac xuat cua truong thoi gian cho normal log
     ptime_normal.append(float(i)/float(normal_line))
+for i in count_time_normal_items:         # fix 14.9
+    time_label_normal.append(i[0])
+list_ptime_normal = dict(zip(time_list_normal,ptime_normal)) # fix 14.9
+for i in time_list_normal:
+    for j in range(len(list_ptime_normal.keys())):
+        if i == list_ptime_normal.keys()[j]:
+            time_compare_normal.append(list_ptime_normal.values()[j])
+# pprint (time_compare_normal)
+# pprint(time_compare_normal)
+#xac suat cua truong method cho normal log va so sanh gia tri xac suat voi thoi gian tuong ug
 for i in count_method_normal:
     pmethod_normal.append(float(i)/float(normal_line))
+for i in count_method_normal_items:         # fix 14.9
+    method_label_normal.append(i[0])
+list_pmethod_normal = dict(zip(method_list_normal,pmethod_normal)) # fix 14.9
+for i in method_list_normal:
+    for j in range(len(list_pmethod_normal.keys())):
+        if i == list_pmethod_normal.keys()[j]:
+            method_compare_normal.append(list_pmethod_normal.values()[j])
+# pprint (method_compare_normal)
 for i in count_query_normal:
     pquery_normal.append(float(i)/float(normal_line))
+for i in count_query_normal_items:         
+    query_label_normal.append(i[0])
+list_pquery_normal = dict(zip(query_list_normal,pquery_normal))
+for i in query_list_normal:
+    for j in range(len(list_pquery_normal.keys())):
+        if i == list_pquery_normal.keys()[j]:
+            query_compare_normal.append(list_pquery_normal.values()[j])
+# pprint(query_compare_normal)
+# pprint(query_list_normal)
+
 for i in count_status_normal:
     pstatus_normal.append(float(i)/float(normal_line))
+for i in count_status_normal_items:        
+    status_label_normal.append(i[0])
+list_pstatus_code_normal = dict(zip(status_label_normal,pstatus_normal)) # fix 14.9
+for i in status_list_normal:
+    for j in range(len(list_pstatus_code_normal.keys())):
+        if i == list_pstatus_code_normal.keys()[j]:
+            status_compare_normal.append(list_pstatus_code_normal.values()[j])
 csv_normalfile.close()
-
