ebb41eed726d33ff84585c8c1199b142403b7882
phucuong0201 <phucuong0201@gmail.com>
2016-09-22 15:56:30 +0700

    Hoan thanh viec phan loai log doi voi tung dong

--- nb.py
+++ nb.py
@@ -54,8 +54,8 @@ with open('log.csv' , "r+") as csvlogfile:
         if log_line[x] == '0':
             normal_line +=1
     total_line = attack_line + normal_line
-    pattack = float(attack_line)/float(total_line)
-    pnormal = float(normal_line)/float(total_line)
+    pattacklog = float(attack_line)/float(total_line)
+    pnormallog = float(normal_line)/float(total_line)
 csvlogfile.close()
 
 with open('attack_log.csv' , "r+") as csv_attackfile:
@@ -103,7 +103,7 @@ for i in query_list_attack:
             query_compare_attack.append(list_pquery_attack.values()[j])
 for i in count_status_attack:
     pstatus_attack.append(float(i)/float(attack_line))
-for i in count_status_attack_items:         # fix 14.9
+for i in count_status_attack_items:         
     status_label_attack.append(i[0])
 list_pstatus_code_attack = dict(zip(status_label_attack,pstatus_attack)) # fix 14.9
 for i in status_list_attack:
@@ -128,22 +128,20 @@ count_query_normal_items = Counter(query_list_normal).items()
 count_status_normal = Counter(status_list_normal).values()  #status
 count_status_normal_items = Counter(status_list_normal).items()
 
-#xac suat cua truong thoi gian cho normal log va so sanh gia tri xac suat voi thoi gian tuong ung
 for i in count_time_normal:
     ptime_normal.append(float(i)/float(normal_line))
-for i in count_time_normal_items:         # fix 14.9
+for i in count_time_normal_items:         
     time_label_normal.append(i[0])
-list_ptime_normal = dict(zip(time_list_normal,ptime_normal)) # fix 14.9
+list_ptime_normal = dict(zip(time_list_normal,ptime_normal)) 
 for i in time_list_normal:
     for j in range(len(list_ptime_normal.keys())):
         if i == list_ptime_normal.keys()[j]:
             time_compare_normal.append(list_ptime_normal.values()[j])
-#xac suat cua truong method cho normal log va so sanh gia tri xac suat voi thoi gian tuong ug
 for i in count_method_normal:
     pmethod_normal.append(float(i)/float(normal_line))
-for i in count_method_normal_items:         # fix 14.9
+for i in count_method_normal_items:        
     method_label_normal.append(i[0])
-list_pmethod_normal = dict(zip(method_list_normal,pmethod_normal)) # fix 14.9
+list_pmethod_normal = dict(zip(method_list_normal,pmethod_normal))
 for i in method_list_normal:
     for j in range(len(list_pmethod_normal.keys())):
         if i == list_pmethod_normal.keys()[j]:
@@ -161,7 +159,7 @@ for i in count_status_normal:
     pstatus_normal.append(float(i)/float(normal_line))
 for i in count_status_normal_items:        
     status_label_normal.append(i[0])
-list_pstatus_code_normal = dict(zip(status_label_normal,pstatus_normal)) # fix 14.9
+list_pstatus_code_normal = dict(zip(status_label_normal,pstatus_normal)) 
 for i in status_list_normal:
     for j in range(len(list_pstatus_code_normal.keys())):
         if i == list_pstatus_code_normal.keys()[j]:
@@ -173,11 +171,13 @@ method_attack = []
 query_attack = []
 status_code_attack = []
 pattack = []
+plog_in_attack = []
 time_normal = []
 method_normal = []
 query_normal = []
 status_code_normal = []
 pnormal = []
+classify= []
 with open('log_to_classify.csv',"r+") as csv_logfile:
     reader_classifylog = csv.reader(csv_logfile)
     for line in reader_classifylog:
@@ -203,8 +203,7 @@ with open('log_to_classify.csv',"r+") as csv_logfile:
                     status_code_normal.append(list_pstatus_code_normal.values()[j])
         group_pnormal = zip(time_normal, method_normal, query_normal, status_code_normal)
         for i in group_pnormal:
-            pnormal.append(i[0]*i[1]*i[2]*i[3])
-#xac suat dong log duoc phan loai thanh dong log bi tan cong
+            pnormal.append(i[0]*i[1]*i[2]*i[3]*pnormallog)
         for line in time_list_classify:
             for j in range(len(list_ptime_attack.keys())):
                 if line == list_ptime_attack.keys()[j]:
@@ -223,11 +222,18 @@ with open('log_to_classify.csv',"r+") as csv_logfile:
                     status_code_attack.append(list_pstatus_code_attack.values()[j])
         group_pattack = zip(time_attack, method_attack, query_attack, status_code_attack)
         for i in group_pattack:
-            pattack.append(i[0]*i[1]*i[2]*i[3])
-
+            pattack.append(i[0]*i[1]*i[2]*i[3]*pattacklog)
         group_plog = zip(pnormal, pattack)
-        for i in group_plog:
-            if i[0] > i[1]:
-                pprint('Dong log khong bi tan cong')
-            elif i[0] < i[1]:
-                pprint('Dong log bi tan cong')
+    for i in group_plog:
+        if i[0] > i[1]:
+            classify.append('Dong log khong bi tan cong')
+        if i[0] < i[1]:
+            classify.append('Dong log bi tan cong')
+
+    with open('log.txt', 'r+') as f:
+        lines = []
+        for line in f:
+            lines.append(line)
+        group_log_classify = zip(lines, classify)
+        pprint(group_log_classify)
+
